39c90f95593e5667100f26ceb08185a10e782a97
Arthur Wolf <wolf@ic-ub24-spr-015.intranet.epfl.ch>
2024-05-08 10:35:34 +0200

    Finish do_read

--- done/imgfs_read.c
+++ done/imgfs_read.c
@@ -1,6 +1,7 @@
 #include "imgfs.h"
 #include "image_content.h"
 
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -21,7 +22,7 @@ int do_read(const char* img_id, int resolution, char** image_buffer,
     M_REQUIRE_NON_NULL(image_size);
     M_REQUIRE_NON_NULL(imgfs_file);
 
-    // Find the image in the index
+    // Find the image with the right img_id
     int index = -1;
     for (int i = 0; i < imgfs_file->header.max_files; ++i) {
         if (strncmp(img_id, imgfs_file->metadata[i].img_id, MAX_IMG_ID) == 0) {
@@ -30,18 +31,39 @@ int do_read(const char* img_id, int resolution, char** image_buffer,
         }
     }
 
-    if (index == -1) {
-        return ERR_INVALID_IMGID;
-    }
+    // There is no image with the requested img_id
+    if (index == -1) return ERR_IMAGE_NOT_FOUND;
 
     // Check if the image already exists in the requested resolution
     // If not, resize it to the resolution
-    if (imgfs_file->metadata[index].size[resolution] != resolution) {
+    if (imgfs_file->metadata[index].size[resolution] == 0) {
         lazily_resize(resolution, imgfs_file, index);
     }
 
-    // TODO : FINISH
+    // At this point, the position of the image in the file is known and so is its size
+    size_t offset = imgfs_file->metadata[index].offset[resolution];
+    size_t size = imgfs_file->metadata[index].size[resolution];
 
+    // Allocate memory for the image content
+    *image_buffer = calloc(1, size);
+    if (*image_buffer == NULL) return ERR_OUT_OF_MEMORY;
+    
+
+    // Read the image content from the file
+    // Move the file pointer to the right position
+    if (fseek(imgfs_file->file, (long)offset, SEEK_SET) != 0) {
+        free(*image_buffer);
+        return ERR_IO;
+    }
+
+    // Read the image content into the buffer
+    if (fread(*image_buffer, size, 1, imgfs_file->file) != 1) {
+        free(*image_buffer);
+        return ERR_IO;
+    }
+
+    // Update the output parameter
+    *image_size = size;
 
     return ERR_NONE;
 }
